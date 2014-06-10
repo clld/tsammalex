96ad3838bc92b215e9757fdeac017c389b8b3a7c
xrotwang <xrotwang@googlemail.com>
2014-06-10 23:19:00 +0200

    more tests

--- tsammalex/tests/test_models.py
+++ tsammalex/tests/test_models.py
@@ -1,10 +1,11 @@
 from clld.tests.util import TestWithEnv
 from clld.db.models import common
+from clld.db.meta import DBSession
 
 
 class Tests(TestWithEnv):
     def test_Word(self):
-        from tsammalex.models import Word
+        from tsammalex.models import Word, Variety, Bibrec
 
         class Data(dict):
             def add(self, *args, **kw):
@@ -17,11 +18,16 @@ class Tests(TestWithEnv):
             ValueSet=dict(vs=vs),
             Languoid=dict(l=vs.language),
             Species=dict(p=vs.parameter),
-            Contribution=dict(tsammalex=common.Contribution(id='c')))
+            Contribution=dict(tsammalex=common.Contribution(id='c')),
+            Variety={v.id: v for v in DBSession.query(Variety)},
+            Bibrec={v.id: v for v in DBSession.query(Bibrec)})
         l = Word(id='w', valueset=vs)
         row = l.to_csv()
         l2 = Word.from_csv(row, data=data)
         self.assertEquals(l2.id, l.id)
+        row[8] = 'ngh-e'
+        row[10] = 'picker2002[12]'
+        l2 = Word.from_csv(row, data=data)
 
     def test_Variety(self):
         from tsammalex.models import Variety
@@ -42,12 +48,21 @@ class Tests(TestWithEnv):
         self.assertEquals(l2.contributor.name, l.contributor.name)
 
     def test_Species(self):
-        from tsammalex.models import Species
+        from tsammalex.models import Species, Category, Ecoregion, Country
 
         l = Species(id='l')
         row = l.to_csv()
         l2 = Species.from_csv(row)
         self.assertEquals(l2.id, l.id)
+
+        row = ['id', 'name', 'description', None, None, None, None]
+        data = {}
+        for cls in [Country, Category, Ecoregion]:
+            data[cls.mapper_name()] = {}
+            for i, obj in enumerate(DBSession.query(cls)):
+                data[cls.mapper_name()][obj.id] = obj
+            row.append(','.join(data[cls.mapper_name()].keys()))
+        s = Species.from_csv(row, data=data)
 
     def test_Languoid(self):
         from tsammalex.models import Languoid
@@ -57,3 +72,10 @@ class Tests(TestWithEnv):
         l2 = Languoid.from_csv(row)
         self.assertEquals(l2.id, l.id)
         self.assertAlmostEqual(l.latitude, l2.latitude)
+
+    def test_misc(self):
+        from tsammalex.models import Category, Ecoregion, Country
+
+        for cls in [Category, Ecoregion, Country]:
+            obj = cls.first()
+            assert obj.csv_head()
